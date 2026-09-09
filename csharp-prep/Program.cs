dedb9cd640d2580dce946026ec9ad9485bcfdfe3
lanre4444 <lanreojo66@gmail.com>
2026-09-09 13:27:44 +0100

    Save current C# prep work

---
+++ csharp-prep/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.ComponentModel;
+
+Console.Write("What is your first name? ");
+string first = Console.ReadLine();
+
+Console.Write("What is your last name? ");
+string last = Console.ReadLine();
